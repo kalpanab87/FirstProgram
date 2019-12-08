77aeff74e287e8bdb058e14442e3614fa1a12cc2
Kalpanab87 <kalpana7.ece@gmail.com>
2019-12-08 15:35:05 +0000

    my first commit

---
+++ src/learnprogramming/HelloWorld.java
@@ -0,0 +1,8 @@
+package learnprogramming;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("First Java Program");
+    }
+}
